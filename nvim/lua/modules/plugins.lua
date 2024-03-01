431a56e27f9d801737de2fe7b18c35e7f49c9122
davipolita <davipolita@weg.net>
2024-03-01 13:21:12 -0300

    update lsp

--- nvim/lua/modules/plugins.lua
+++ nvim/lua/modules/plugins.lua
@@ -14,12 +14,13 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
         -- or                              , branch = '0.1.1',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    { "catppuccin/nvim", name = "catppuccin" },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    { "catppuccin/nvim",                 name = "catppuccin" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     {
         "nvim-tree/nvim-tree.lua",
         version = "*",
@@ -27,7 +28,7 @@ local plugins = {
             "nvim-tree/nvim-web-devicons",
         },
     },
-    {"tpope/vim-fugitive"},
+    { "tpope/vim-fugitive" },
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
@@ -37,19 +38,29 @@ local plugins = {
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                            -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            {
+                "L3MON4D3/LuaSnip",
+                -- follow latest release.
+                version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+                -- install jsregexp (optional!).
+                build = "make install_jsregexp"
+            },
         }
     },
     {
@@ -68,9 +79,9 @@ local plugins = {
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     },
-    {'akinsho/toggleterm.nvim', version = "*", config = true}
-
+    { 'akinsho/toggleterm.nvim', version = "*", config = true },
+    { 'stevearc/conform.nvim',   opts = {}, }
 }
 local opts = {}
 
-require("lazy").setup(plugins, opts )
+require("lazy").setup(plugins, opts)
